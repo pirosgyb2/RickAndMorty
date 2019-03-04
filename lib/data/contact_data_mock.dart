45156fa441e7b69693280df71f9a88fe4062af0c
pirosgyb2 <pirosgyb2@users.noreply.github.com>
2019-03-04 12:13:12 +0100

    Base sample project from tutorial

---
+++ lib/data/contact_data_mock.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'contact_data.dart';
+
+class MockContactRepository implements ContactRepository {
+  @override
+  Future<List<Contact>> fetch() {
+    return Future.value(kContacts);
+  }
+}
+
+const kContacts = const <Contact>[
+  const Contact(
+      fullName: 'Romain Hoogmoed', email: 'romain.hoogmoed@example.com'),
+  const Contact(fullName: 'Emilie Olsen', email: 'emilie.olsen@example.com')
+];
---
+++ lib/injection/dependency_injection.dart
@@ -0,0 +1,29 @@
+import '../data/contact_data.dart';
+import '../data/contact_data_impl.dart';
+import '../data/contact_data_mock.dart';
+
+enum Flavor { MOCK, PRO }
+
+class Injector {
+  static final Injector _singleton = Injector._internal();
+  static Flavor _flavor;
+
+  static void configure(Flavor flavor) {
+    _flavor = flavor;
+  }
+
+  factory Injector() {
+    return _singleton;
+  }
+
+  Injector._internal();
+
+  ContactRepository get contactRepository {
+    switch (_flavor) {
+      case Flavor.MOCK:
+        return new MockContactRepository();
+      default: //Flavor.PRO
+        return new RandomUserRepository();
+    }
+  }
+}
